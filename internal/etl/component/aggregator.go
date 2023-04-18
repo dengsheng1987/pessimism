047aaee452581bb6aa93b9ced37b3f750df8f4f8
Ethen Pociask <ethen.pociask@coinbase.com>
2023-04-18 14:53:26 -0700

    [12-no-aggregation-component-support] Light implementation of aggregator component

--- internal/etl/component/aggregator.go
+++ internal/etl/component/aggregator.go
@@ -1,4 +1,46 @@
 package component
 
+import (
+	"context"
+
+	"github.com/base-org/pessimism/internal/core"
+	"github.com/base-org/pessimism/internal/logging"
+	"go.uber.org/zap"
+)
+
 // TODO(#12): No Aggregation Component Support
-type Aggregator struct{}
+type Aggregator struct {
+	ctx context.Context
+
+	*metaData
+}
+
+// NewAggregator ... Initializer
+func NewAggregator(ctx context.Context, outType core.RegisterType,
+	od OracleDefinition, opts ...Option) (Component, error) {
+	a := &Aggregator{
+		ctx: ctx,
+
+		metaData: &metaData{
+			id:             core.NilCompID(),
+			cType:          core.Aggregator,
+			egressHandler:  newEgressHandler(),
+			ingressHandler: newIngressHandler(),
+			state:          Inactive,
+			output:         outType,
+		},
+	}
+
+	for _, opt := range opts {
+		opt(a.metaData)
+	}
+
+	if cfgErr := od.ConfigureRoutine(); cfgErr != nil {
+		return nil, cfgErr
+	}
+
+	logging.WithContext(ctx).Info("Constructed component",
+		zap.String("ID", a.metaData.id.String()))
+
+	return a, nil
+}
